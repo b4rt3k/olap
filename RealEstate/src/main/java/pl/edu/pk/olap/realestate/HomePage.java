448f2fc6469a94de1fb54602a6b9aabe2530ca30
b4rt3k <b4rt3k408@o2.pl>
2013-03-10 19:42:02 +0100

    removed unused files

--- RealEstate/src/main/java/pl/edu/pk/olap/realestate/HomePage.java
+++ RealEstate/src/main/java/pl/edu/pk/olap/realestate/HomePage.java
@@ -1,13 +1,12 @@
 package pl.edu.pk.olap.realestate;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-    }
+	}
 }
