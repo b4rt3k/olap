e2e2084c12e060c06e6ea866c74e253d047786d4
b4rt3k <b4rt3k408@o2.pl>
2013-04-02 23:49:11 +0200

    parser refactor

--- RealEstate/src/main/java/pl/edu/pk/olap/realestate/core/parser/ApartmentGuideParser.java
+++ RealEstate/src/main/java/pl/edu/pk/olap/realestate/core/parser/ApartmentGuideParser.java
@@ -1,154 +1,268 @@
 package pl.edu.pk.olap.realestate.core.parser;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.apache.log4j.Logger;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import pl.edu.pk.olap.realestate.config.ConfigReader;
 import pl.edu.pk.olap.realestate.config.ConfigurationConstants;
 
 /**
  * @author b4rt3k
  * 
  */
-public class ApartmentGuideParser implements HttpParser {
+public class ApartmentGuideParser extends AbstractParser {
 
-	@Autowired
-	private ConfigReader reader;
-	private int timeout;
 	private static Logger log = Logger.getLogger(ApartmentGuideParser.class);
 
 	@Override
-	public void parse() {
-		log.info("Apartment Guide parsing started");
-		timeout = Integer.parseInt(reader.getProperty(ConfigurationConstants.HTTP_CONNECTION_TIMEOUT_MILLIS));
-		Document doc = null;
+	public Logger getLogger() {
+		return ApartmentGuideParser.log;
+	}
+
+	@Override
+	public String getPortalName() {
+		return "ApartmentGuide";
+	}
+
+	@Override
+	public String getUrl() {
+		return ConfigurationConstants.APARTMENTGUIDE_URL;
+	}
+
+	@Override
+	public Elements statesLinksSelector(Element root) {
+		return root.select("ul.browse_links>li>a[href]");
+	}
+
+	@Override
+	public Elements citiesLinksSelector(Element root) {
+		return root.select(".padding_box ul>li>a[href]");
+	}
+
+	@Override
+	public Element pagesCountSelector(Element root) {
+		return root.select("div.pagination>ol>li:last-child a").first();
+	}
+
+	@Override
+	public int extractPagesCount(Element e) {
 		try {
-			doc = Jsoup.connect(reader.getProperty(ConfigurationConstants.APARTMENTGUIDE_URL)).timeout(timeout).userAgent("Mozilla").get();
-			Elements links = doc.select("ul.browse_links>li>a[href]");
-			if (links.size() > 0) {
-				log.info("States links found.");
-				for (Element link : links) {
-					parseState(link.attr("abs:href"), link.text());
-				}
-			}
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			e.printStackTrace();
+			return Integer.parseInt(new URL(e.attr("abs:href")).getQuery().split("=")[1]);
+		} catch (NumberFormatException | MalformedURLException e1) {
+			throw new RuntimeException(e1);
 		}
 	}
 
-	private void parseState(String url, String stateName) {
-		log.info("Parsing state: " + stateName);
-		System.out.println("------------------------------------------");
-		System.out.println(stateName);
-		System.out.println("------------------------------------------");
-		Document doc = null;
-		try {
-			doc = Jsoup.connect(url).timeout(timeout).userAgent("Mozilla").get();
-			Elements links = doc.select(".padding_box ul>li>a[href]");
-			if (links.size() > 0) {
-				log.info("Cities links found.");
-				for (Element link : links) {
-					parseCity(link.attr("abs:href"), link.text());
-				}
-			}
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			e.printStackTrace();
-		}
+	@Override
+	public String pageNumberHttpQuery() {
+		return "?page=";
 	}
 
-	private void parseCity(String url, String cityName) {
-		log.info("Parsing city: " + cityName);
-		int pages = 1;
-		boolean resultsFound = false;
-		Document doc = null;
-		try {
-			doc = Jsoup.connect(url).timeout(timeout).userAgent("Mozilla").get();
-			Element elem = doc.select("div.pagination>ol>li").last().select("a").first();
-			if (elem != null) {
-				resultsFound = true;
-				pages = Integer.parseInt(new URL(elem.attr("abs:href")).getQuery().split("=")[1]);
-				log.info("Pages count found: " + pages);
-			}
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			e.printStackTrace();
-		}
-
-		if (resultsFound) {
-			for (int i = 0; i < pages; i++) {
-				parsePage(url + "?page=" + (i + 1), cityName, (i + 1));
-			}
-		}
+	@Override
+	public Elements singleItemBaseInfoSelector(Element root) {
+		return root.select("div#results div#resultWrapper div.result");
 	}
 
-	private void parsePage(String url, String city, int page) {
-		log.info("Parsing page: " + page + ", city: " + city);
-		Document doc = null;
-		try {
-			doc = Jsoup.connect(url).timeout(timeout).userAgent("Mozilla").get();
-			Elements items = doc.select("div#results div#resultWrapper div.result");
-			int index = 0;
-			if (items.size() > 0) {
-				log.info("Apartments base info found.");
-				for (Element item : items) {
-					System.out.println("--Name: " + item.select("div.column2>h3>a[href]").text());
-					System.out.println("--Reigon: " + item.select("div.column2>ul>li.display_address>span[itemprop=addressRegion]").text());
-					System.out.println("--Locality: " + item.select("div.column2>ul>li.display_address>span[itemprop=addressLocality]").text());
-					System.out.println("--Postal Code: " + item.select("div.column2>ul>li.display_address>span[itemprop=postalCode]").text());
-					System.out.println("--Phone Number: " + item.select("ul.listing_controls>li.phone_number.large.non_sem_number").text());
-					parseItem(item.select("div.column2>h3>a[href]").attr("abs:href"), index++);
-					System.out.println();
-				}
-			}
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			e.printStackTrace();
-		}
+	@Override
+	public Element itemNameSelector(Element root) {
+		return root.select("div.column2>h3>a[href]").first();
 	}
 
-	private void parseItem(String url, int index) {
-		log.info("Parsing item: " + index);
-		Document doc = null;
-		try {
-			doc = Jsoup.connect(url).timeout(timeout).userAgent("Mozilla").get();
-			Element street = doc.select("div#details_header span[itemprop=streetAddress]").first();
-			System.out.println("--Street: " + street.text().replaceAll(",", ""));
-			Element lastRow = doc.select("div#floorplans_and_pricing_tab table.unit_detail tr").last();
-			if (lastRow != null) {
-				log.info("Apartment details found (floorplan table).");
-				Elements elems = lastRow.select("td");
-				System.out.println("--Style: " + elems.eq(0).text());
-				System.out.println("--Beds: " + elems.eq(1).text());
-				System.out.println("--Bathrooms: " + elems.eq(2).text());
-				System.out.println("--Area [Sq. Ft.]: " + elems.eq(4).text().replaceAll("\\D", ""));
-				System.out.println("--Price: " + elems.eq(5).text().replaceAll("[^\\$|\\.|\\d|-]", ""));
-				System.out.println("--Term: " + elems.eq(6).text());
-				System.out.println("--Deposit: " + elems.eq(7).text().replaceAll("[^\\$|\\.|\\d]", "").trim());
-			}
-			System.out.println("--Features: ");
-			Elements features = doc.select("div#apartment_features_tab ul.features>li");
-			if (features.size() > 0) {
-				log.info("Apartment features found.");
-				for (Element feature : features) {
-					System.out.println("----" + feature.text());
-				}
-			}
-			Element description = doc.select("div#apartment_info_tab div#details_description #listing_description").first();
-			if (description != null) {
-				log.info("Apartment description found.");
-				System.out.println("--Description: " + description.text());
-			}
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			e.printStackTrace();
+	@Override
+	public String extractName(Element e) {
+		if (e != null) {
+			return e.text().trim();
 		}
+		return null;
+	}
+
+	@Override
+	public Element itemLocalitySelector(Element root) {
+		return root.select("div.column2>ul>li.display_address>span[itemprop=addressLocality]").first();
+	}
+
+	@Override
+	public String extractLocality(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemRegionSelector(Element root) {
+		return root.select("div.column2>ul>li.display_address>span[itemprop=addressRegion]").first();
+	}
+
+	@Override
+	public String extractRegion(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemPostalCodeSelector(Element root) {
+		return root.select("div.column2>ul>li.display_address>span[itemprop=postalCode]").first();
+	}
+
+	@Override
+	public String extractPostalCode(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemPhoneNumberSelector(Element root) {
+		return root.select("ul.listing_controls>li.phone_number.large.non_sem_number").first();
+	}
+
+	@Override
+	public String extractPhoneNumber(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemDetailsLinkSelector(Element root) {
+		return root.select("div.column2>h3>a[href]").first();
+	}
+
+	@Override
+	public Element itemStreetSelector(Element root) {
+		return root.select("div#details_header span[itemprop=streetAddress]").first();
+	}
+
+	@Override
+	public String extractStreet(Element e) {
+		if (e != null) {
+			return e.text().trim().replaceAll(",", "");
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemStyleSelector(Element root) {
+		return root.select("div#floorplans_and_pricing_tab table.unit_detail tr:last-child td:eq(0)").first();
+	}
+
+	@Override
+	public String extractStyle(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemBedsCountSelector(Element root) {
+		return root.select("div#floorplans_and_pricing_tab table.unit_detail tr:last-child td:eq(1)").first();
+	}
+
+	@Override
+	public String extractBedsCount(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemBathroomsCountSelector(Element root) {
+		return root.select("div#floorplans_and_pricing_tab table.unit_detail tr:last-child td:eq(2)").first();
+	}
+
+	@Override
+	public String extractBathroomsCount(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemAreaSelector(Element root) {
+		return root.select("div#floorplans_and_pricing_tab table.unit_detail tr:last-child td:eq(4)").first();
+	}
+
+	@Override
+	public String extractArea(Element e) {
+		if (e != null) {
+			return e.text().trim().replaceAll("\\D", "");
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemPriceSelector(Element root) {
+		return root.select("div#floorplans_and_pricing_tab table.unit_detail tr:last-child td:eq(5)").first();
+	}
+
+	@Override
+	public String extractPrice(Element e) {
+		if (e != null) {
+			return e.text().trim().replaceAll("[^\\$|\\.|\\d|-]", "");
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemTermSelector(Element root) {
+		return root.select("div#floorplans_and_pricing_tab table.unit_detail tr:last-child td:eq(6)").first();
+	}
+
+	@Override
+	public String extractTerm(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemDepositSelector(Element root) {
+		return root.select("div#floorplans_and_pricing_tab table.unit_detail tr:last-child td:eq(7)").first();
+	}
+
+	@Override
+	public String extractDeposit(Element e) {
+		if (e != null) {
+			return e.text().trim().replaceAll("[^\\$|\\.|\\d]", "");
+		}
+		return null;
+	}
+
+	@Override
+	public Elements itemFeaturesSelector(Element root) {
+		return root.select("div#apartment_features_tab ul.features>li");
+	}
+
+	@Override
+	public String extractFeature(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
+	}
+
+	@Override
+	public Element itemDescriptionSelector(Element root) {
+		return root.select("div#apartment_info_tab div#details_description #listing_description").first();
+	}
+
+	@Override
+	public String extractDescription(Element e) {
+		if (e != null) {
+			return e.text().trim();
+		}
+		return null;
 	}
 }
